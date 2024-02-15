9c4b516ca5d6922d1061979f5ebc26d505ed36ed
salembht <127092764+salembht@users.noreply.github.com>
2024-02-16 00:50:49 +0300

    Add project files.

---
+++ BlazorCurdDotnet8/BlazorCurdDotnet8/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCurdDotnet8.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCurdDotnet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+        : base(options) 
+        {
+        
+        }
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ BlazorCurdDotnet8/BlazorCurdDotnet8/Program.cs
@@ -0,0 +1,37 @@
+using BlazorCurdDotnet8.Client.Pages;
+using BlazorCurdDotnet8.Components;
+using BlazorCurdDotnet8.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorCurdDotnet8.Client._Imports).Assembly);
+
+app.Run();
